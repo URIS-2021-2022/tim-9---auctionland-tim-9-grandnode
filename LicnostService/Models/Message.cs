f1ac0e2afa36415d761a3c347406b4da75af75f2
MilunovicN99 <57875777+MilunovicN99@users.noreply.github.com>
2022-02-20 01:06:14 +0100

    Pozivanje loggera

---
+++ LicnostService/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LicnostService.Models
+{
+    public class Message
+    {
+        public string ServiceName { get; set; }
+        public string Method { get; set; }
+        public string Information { get; set; }
+        public string Error { get; set; }
+    }
+}
---
+++ LicnostService/ServiceCalls/ILoggerService.cs
@@ -0,0 +1,13 @@
+﻿using LicnostService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LicnostService.ServiceCalls
+{
+    interface ILoggerService
+    {
+        void CreateMessage(Message message);
+    }
+}
