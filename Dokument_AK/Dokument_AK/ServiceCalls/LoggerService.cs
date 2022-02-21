5c32abeb78b6fe0e2fe0bce45450359955e395a8
aleksakomosar <aleksakomosar@gmail.com>
2022-02-21 13:37:27 +0100

    Popravka logera
    Dodan try i catch za pokretanje bez logera

--- Dokument_AK/Dokument_AK/ServiceCalls/LoggerService.cs
+++ Dokument_AK/Dokument_AK/ServiceCalls/LoggerService.cs
@@ -30,15 +30,22 @@ namespace Dokument_AK.ServiceCalls
         /// <param name="message"></param>
         public void CreateMessage(Message message)
         {
-            using (HttpClient client = new HttpClient())
+            try
             {
-                var x = configuration["Services:LoggerService"];    //Services:LoggerService je definisano u appsettings.json i sadrži lokaciju servisa
-                Uri url = new Uri($"{ configuration["Services:LoggerService"]}api/logger");
+                using (HttpClient client = new HttpClient())
+                {
+                    var x = configuration["Services:LoggerService"];    //Services:LoggerService je definisano u appsettings.json i sadrži lokaciju servisa
+                    Uri url = new Uri($"{ configuration["Services:LoggerService"]}api/logger");
 
-                HttpContent content = new StringContent(JsonConvert.SerializeObject(message));
-                content.Headers.ContentType.MediaType = "application/json";
+                    HttpContent content = new StringContent(JsonConvert.SerializeObject(message));
+                    content.Headers.ContentType.MediaType = "application/json";
 
-                HttpResponseMessage response = client.PostAsync(url, content).Result;
+                    HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+                }
+            }
+            catch
+            {
 
             }
         }
