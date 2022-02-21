8c5d5b1c7806476b94c3590f8cb2aca7ce66caba
sarakijanovic <sara.kijanovic@unc.ac.rs>
2022-02-21 14:06:54 +0100

    logger1
    logger1

---
+++ OvlascenoLice/OvlascenoLice/ServiceCalls/ILoggerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OvlascenoLice.ServiceCalls
+{
+    
+       public interface ILoggerService
+        {
+            void CreateMessage(Message message);
+        }
+    
+}
